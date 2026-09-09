bcc2421202b9686534845598b658694bba1cd1d3
John WOODEN <29alistergeo.sebastian@wollemi.nsw.edu>
2026-09-10 09:36:06 +1000

    made the formating better and added a main funtion back

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-from stats import chars_dict_to_sorted_list, get_chars_dict, get_num_words
 import sys
 
-print("hello")
+from stats import (
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+    get_num_words,
+)
 
-if len(sys.argv) < 2:
-    print("Usage: python3 main.py <path_to_book>")
-    sys.exit(1)
 
-book_path = sys.argv[1]
+def main() -> None:
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
 
-#the above is setting up the agruments and the book path using an import "sys"
-
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 
 def get_book_text(path: str) -> str:
@@ -18,21 +25,20 @@ def get_book_text(path: str) -> str:
         return f.read()
 
 
-def print_report(get_num_words,chars_dict_to_sorted_list,get_chars_dict):
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    chars_dict = get_chars_dict(text)
-    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
-    num_words = get_num_words(text)
+def print_report(
+    book_path: str, num_words: int, chars_sorted_list: list[tuple[str, int]]
+) -> None:
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-    for char in chars_sorted_list:
-        if char[0].isalpha():
-            print(f"{char[0]}: {char[1]}")
+    for char, count in chars_sorted_list:
+        if not char.isalpha():
+            continue
+        print(f"{char}: {count}")
+
     print("============= END ===============")
 
 
-print_report(get_num_words,chars_dict_to_sorted_list,get_chars_dict)
+main()
